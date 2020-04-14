2a04ac8e2b661fa23b49aa5a30403480200e2b5e
JoaoBrux <63651320+JoaoBrux@users.noreply.github.com>
2020-04-14 14:48:23 -0300

    Add files via upload

---
+++ Exerciciio2.java
@@ -0,0 +1,46 @@
+package exercicio;
+
+import java.util.Scanner;
+
+public class Exerciciio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		
+		Scanner input = new Scanner (System.in);
+		
+		int numb1;
+		int numb2;
+		
+		System.out.print("Digite o primeiro número:");
+		numb1 = input.nextInt();
+		
+		System.out.print("Digite o segundo número:");
+		numb2 = input.nextInt();
+	
+		System.out.println(numb1 + "+" + numb2 + "=" + (numb1 + numb2));
+		System.out.println(numb1 + "-" + numb2 + "=" + (numb1 - numb2));
+		System.out.println(numb1 + "*" + numb2 + "=" + (numb1 * numb2));
+		System.out.println(numb1 + "/" + numb2 + "=" + (numb1 / numb2));
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
